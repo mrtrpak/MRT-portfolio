99660359d4ad3a67c96731b8cce08b9b328d7a11
Michael Trpak <mrtrpak3@gmail.com>
2021-07-29 16:02:22 -0400

    shortening code and adding header to app

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import bgImage from './assets/mntsBG.jpg';
+import Header from './components/header/header.component';
 import HomePage from './pages/home/homepage.component';
 
 function App() {
   return (
-    <div>
+    <div style={{
+      backgroundImage: `url(${bgImage})`,
+      height: "100vh",
+      width: "100%",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      backgroundPosition: "center"
+    }}>
+      <Header />
       <Switch>
-        <div style={{
-          backgroundImage: `url(${bgImage})`,
-          height: "100vh",
-          width: "100%",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "center"
-        }}>
-          <Route exact path="/" component={HomePage} />
-        </div>
+        <Route exact path="/" component={HomePage} />
       </Switch>
     </div>
   );
