e5954667cb50fa18b110250f4c05cdb6497645e9
Michael Trpak <mrtrpak3@gmail.com>
2021-07-29 15:24:41 -0400

    finished editing home page with animation styles on load

--- src/pages/home/homepage.component.jsx
+++ src/pages/home/homepage.component.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 import './homepage.styles.scss';
 
 const HomePage = () => (
-  <div>
+  <div className="home-page">
     <h1 className="home-name">
-      I AM <span className="my-name">MICHAEL TRPAK</span>
+      { (window.innerWidth > 950) ? "I AM" : ""}
+      <span className="my-name"> MICHAEL TRPAK</span>
     </h1>
     <h2 className="home-title">
-      FULL STACK WEB DEVELOPER
+    { (window.innerWidth > 950) ? "FULL STACK WEB DEVELOPER" : "WEB DEVELOPER"}
     </h2>
   </div>
 );
