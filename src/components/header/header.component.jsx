8e184633c42bb8ab7bf7792e0539f9b84209b5ea
Michael Trpak <mrtrpak3@gmail.com>
2021-07-29 16:39:05 -0400

    styled header and added to component & fixed error in manifest

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './header.styles.scss';
 
 const Header = () => (
-  <div>
-    header
+  <div className="header">
+    <div className="options">
+      <Link className="option" to="/">HOME</Link>
+      <Link className="option" to="/hobbies">HOBBIES</Link>
+      <Link className="option" to="/projects">PROJECTS</Link>
+      <Link className="option" to="skills">SKILLS</Link>
+    </div>
   </div>
 );
 
