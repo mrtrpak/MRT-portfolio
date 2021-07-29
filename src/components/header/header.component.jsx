c12c824a651b44a383a243f799755c77d37c483e
Michael Trpak <mrtrpak3@gmail.com>
2021-07-29 16:44:35 -0400

    added react-icons

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { GoThreeBars } from 'react-icons/go';
 
 import './header.styles.scss';
 
@@ -10,6 +11,7 @@ const Header = () => (
       <Link className="option" to="/hobbies">HOBBIES</Link>
       <Link className="option" to="/projects">PROJECTS</Link>
       <Link className="option" to="skills">SKILLS</Link>
+      <GoThreeBars />
     </div>
   </div>
 );
