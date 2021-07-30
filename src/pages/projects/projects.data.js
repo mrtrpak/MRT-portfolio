ed47c85d40a6a0c6b3adca586bbbddefca63b4d4
Michael Trpak <mrtrpak3@gmail.com>
2021-07-30 15:57:27 -0400

    adding projects data

---
+++ src/pages/projects/projects.data.js
@@ -0,0 +1,24 @@
+import brainGif from '../../assets/gifs/brainFace.gif';
+import businessGif from '../../assets/gifs/businessCard.gif';
+import eagleGif from '../../assets/gifs/eagle-outfitters.gif';
+
+export const projects = {
+  0: {
+    title: "Eagle Outfitters",
+    clip: eagleGif,
+    url: "https://eagle-outfitters.herokuapp.com/",
+    gitUrl: "https://github.com/mrtrpak/eagle-outfitters"
+  },
+  1: {
+    title: "Face Recognition",
+    clip: brainGif,
+    url: "https://smart-brain-detect3.herokuapp.com/",
+    gitUrl: "https://github.com/mrtrpak/facerecognition"
+  },
+  2: {
+    title: "Business Cards",
+    clip: businessGif,
+    url: "https://stormy-harbor-93453.herokuapp.com/",
+    gitUrl: "https://github.com/mrtrpak/Futurum-est-nostrum"
+  }
+};
