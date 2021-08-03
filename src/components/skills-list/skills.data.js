c8c2ad733d5cafa829260010f9a8403f978227a5
Michael Trpak <mrtrpak3@gmail.com>
2021-08-03 16:42:52 -0400

    adding skills list in js file

---
+++ src/components/skills-list/skills.data.js
@@ -0,0 +1,22 @@
+export const skills = [
+  {
+    id: 1,
+    name: "Technical Skills",
+    list: [
+      "React", 
+      "Javascript", 
+      "CSS3",
+      "NodeJS",
+      "Redux",
+      "Bootstrap",
+      "Foundation",
+      "MySQL",
+      "JQuery",
+      "RESTful API",
+      "Firebase",
+      "MongoDB",
+      "Express.js",
+      "Heroku"
+    ]
+  }
+]
