7b6735923b1719fad2108ddf308f629ab1adb868
Michael Trpak <mrtrpak3@gmail.com>
2021-08-03 14:51:10 -0400

    adding skills list component and adding margin top to skills page

---
+++ src/components/skills-list/skills-list.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './skills-list.styles.scss';
+
+const SkillsList = () => {
+
+  return (
+    <div className="skills-list">
+
+    </div>
+  );
+};
+
+export default SkillsList;
