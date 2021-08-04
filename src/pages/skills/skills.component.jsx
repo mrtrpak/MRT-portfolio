bf45fca12d1e60e1a8ea7c1c3f9fcd00c3e38072
Michael Trpak <mrtrpak3@gmail.com>
2021-08-04 15:18:58 -0400

    importing in skills list

--- src/pages/skills/skills.component.jsx
+++ src/pages/skills/skills.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './skills.styles.scss';
 
+import SkillsList from '../../components/skills-list/skills-list.component';
 import SkillIcons from '../../components/skill-icons/skill-icons.component';
 
 const SkillsPage = () => (
   <div className="skills-page">
+    <SkillsList />
     <SkillIcons />
   </div>
 );
