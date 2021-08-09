58b3b79257bc059fdf9985db8eb283c5acbcbdd3
Michael Trpak <mrtrpak3@gmail.com>
2021-08-09 12:48:22 -0400

    updates

--- src/pages/soccer/soccer.component.jsx
+++ src/pages/soccer/soccer.component.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 
 import './soccer.styles.scss';
 
@@ -7,24 +6,21 @@ import { soccerToken } from '../../utils/secret';
 
 const SoccerPage = () => {
   const [response, setResponse] = useState({});
-  const [league, setLeague] = useState("BL1");
-
-  const 
   
-  useEffect(() => {
-    fetch("https://api.football-data.org/v2/competitions/BL1/standings", {
-      method: "GET",
-      headers: { "X-Auth-Token": soccerToken }
-    })
-    .then(response => response.json())
-    .then(json => {
-      console.log(json.standings[0].table);
-      Object.entries(json.standings[0].table).map(team => {
-        return console.log(team[1].team.name, "hello object keys");
-      })
-    })
-    .catch(err => console.log(err))
-  }, [response]);
+  // useEffect(() => {
+  //   fetch("https://api.football-data.org/v2/competitions/BL1/standings", {
+  //     method: "GET",
+  //     headers: { "X-Auth-Token": soccerToken }
+  //   })
+  //   .then(response => response.json())
+  //   .then(json => {
+  //     console.log(json.standings[0].table);
+  //     Object.entries(json.standings[0].table).map(team => {
+  //       return console.log(team[1].team.name, "hello object keys");
+  //     })
+  //   })
+  //   .catch(err => console.log(err))
+  // }, [response]);
 
   return (
     <ul>
