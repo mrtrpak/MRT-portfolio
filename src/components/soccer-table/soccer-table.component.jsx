4ad1b7b5f60236ec8ec36cf6b7b300e33f83f9f5
Michael Trpak <mrtrpak3@gmail.com>
2021-08-10 17:43:18 -0400

    making changes to adjust how info is saved

--- src/components/soccer-table/soccer-table.component.jsx
+++ src/components/soccer-table/soccer-table.component.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 
 import './soccer-table.styles.scss';
+
+import { soccerToken } from '../../utils/secret';
 
 const SoccerTable = (props) => {
   const { code } = props;
@@ -8,13 +10,37 @@ const SoccerTable = (props) => {
     "POS", "TEAM NAME", "GP", "W", "D", "L", "P", "GF", "GA", "GD"
   ];
 
+  const [standingsInfo, setStandingsInfo] = useState({
+    0: {}, 1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {}, 8: {}, 9: {},
+    10: {}, 11: {}, 12: {}, 13: {}, 14: {}, 15: {}, 16: {}, 17: {}, 18: {},
+    19: {}, 20: {}
+  });
+
+  useEffect(() => {
+    fetch(`https://api.football-data.org/v2/competitions/${code}/standings`,
+    { method: "GET",  headers: { "X-Auth-Token": soccerToken }})
+    .then(response => response.json())
+    .then(json => Object.entries(json.standings[0].table).map(
+      (team, idx) => (
+        setStandingsInfo({ ...standingsInfo, [idx]: {team}})
+      )
+    .then(console.log(standingsInfo))
+    ))
+    .catch(err => console.log(err));
+  });
+
   return (
     <table className="soccer-table">
-      <tr className="soccer-headers-row">
-        { tableHeaders.map(
-          header => (<th className="soccer-header">{header}</th>)
-        )}
-      </tr>
+      <thead className="soccer-headers-row">
+        <tr>
+          { tableHeaders.map(
+            header => (<th key={header} className="soccer-header">{header}</th>)
+          )}
+        </tr>
+      </thead>
+      <tbody>
+        
+      </tbody>
     </table>
   );
 };
