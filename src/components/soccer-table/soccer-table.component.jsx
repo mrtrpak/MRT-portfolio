29a0e731df437c0f060e4c4c6a301a6abe62b5c9
Michael Trpak <mrtrpak3@gmail.com>
2021-08-11 17:31:50 -0400

    updates

--- src/components/soccer-table/soccer-table.component.jsx
+++ src/components/soccer-table/soccer-table.component.jsx
@@ -46,18 +46,3 @@ const SoccerTable = (props) => {
 };
 
 export default SoccerTable;
-
- // useEffect(() => {
-  //   fetch("https://api.football-data.org/v2/competitions/BL1/standings", {
-  //     method: "GET",
-  //     headers: { "X-Auth-Token": soccerToken }
-  //   })
-  //   .then(response => response.json())
-  //   .then(json => {
-  //     console.log(json.standings[0].table);
-  //     Object.entries(json.standings[0].table).map(team => {
-  //       return console.log(team[1].team.name, "hello object keys");
-  //     })
-  //   })
-  //   .catch(err => console.log(err))
-  // }, [response]);
