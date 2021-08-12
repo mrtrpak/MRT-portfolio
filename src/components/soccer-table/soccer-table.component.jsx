630ea737a4e0800ccddf19094bde34fe56202c72
Michael Trpak <mrtrpak3@gmail.com>
2021-08-12 15:01:59 -0400

    able to get the info I need from api and able to save into state hooks. also moved out headers to its own component

--- src/components/soccer-table/soccer-table.component.jsx
+++ src/components/soccer-table/soccer-table.component.jsx
@@ -4,42 +4,46 @@ import './soccer-table.styles.scss';
 
 import { soccerToken } from '../../utils/secret';
 
+import TableHeaders from '../table-headers/table-headers.component';
+
 const SoccerTable = (props) => {
   const { code } = props;
-  const tableHeaders = [
-    "POS", "TEAM NAME", "GP", "W", "D", "L", "P", "GF", "GA", "GD"
-  ];
+  
 
-  const [standingsInfo, setStandingsInfo] = useState({
-    0: {}, 1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {}, 8: {}, 9: {},
-    10: {}, 11: {}, 12: {}, 13: {}, 14: {}, 15: {}, 16: {}, 17: {}, 18: {},
-    19: {}, 20: {}
-  });
+  const [standingsInfo, setStandingsInfo] = useState({ table: {}});
+  const [isMounted, setIsMounted] = useState(false);
 
-  // useEffect(() => {
-  //   fetch(`https://api.football-data.org/v2/competitions/${code}/standings`,
-  //   { method: "GET",  headers: { "X-Auth-Token": soccerToken }})
-  //   .then(response => response.json())
-  //   .then(json => Object.entries(json.standings[0].table).map(
-  //     (team, idx) => (
-  //       setStandingsInfo({ ...standingsInfo, [idx]: {team}})
-  //     )
-  //   .then(console.log(standingsInfo))
-  //   ))
-  //   .catch(err => console.log(err));
-  // });
+  useEffect(() => {
+    setIsMounted(false);
+
+    const fetchData = async () => {
+      await fetch(
+        `https://api.football-data.org/v2/competitions/${code}/standings`,
+        { method: "GET",  headers: { "X-Auth-Token": soccerToken }})
+        .then(response => response.json())
+        .then(json => setStandingsInfo({ table: json.standings[0].table }))
+        .catch(err => console.log(err));
+      };
+
+      fetchData();
+      setIsMounted(true);
+
+      console.log(standingsInfo);
+  }, [code]);
 
   return (
     <table className="soccer-table">
-      <thead className="soccer-headers-row">
-        <tr>
-          { tableHeaders.map(
-            header => (<th key={header} className="soccer-header">{header}</th>)
-          )}
-        </tr>
-      </thead>
+      <TableHeaders />
       <tbody>
-        
+        { !isMounted ? <tr></tr> : 
+          Object.entries(standingsInfo.table).map((team, idx) => {
+            return (
+              <tr key={idx}>
+                {console.log(team[1].team.name)}
+              </tr>
+            )
+          })
+        }
       </tbody>
     </table>
   );
---
+++ src/components/table-headers/table-headers.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './table-headers.styles.scss';
+
+const TableHeaders = () => {
+  const tableHeaders = [
+    "pos", "team name", "badge", "gp", "w", "d", "l", "pts", "gf", "ga", "gd"
+  ];
+
+  return (
+    <thead>
+      <tr className="soccer-headers-row">
+        { tableHeaders.map(
+          header => (<th key={header} className={`soccer-header ${header}`}>{header.toUpperCase()}</th>)
+        )}
+      </tr>
+    </thead>
+  );
+};
+
+export default TableHeaders;
