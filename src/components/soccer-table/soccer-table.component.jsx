6b5a0d6bd10883e3b4229522c67433d3d3245e9d
Michael Trpak <mrtrpak3@gmail.com>
2021-08-12 16:16:35 -0400

    making updates to styling and rendering

--- src/components/soccer-table/soccer-table.component.jsx
+++ src/components/soccer-table/soccer-table.component.jsx
@@ -35,11 +35,29 @@ const SoccerTable = (props) => {
     <table className="soccer-table">
       <TableHeaders />
       <tbody>
-        { !isMounted ? <tr></tr> : 
+        { !isMounted ? null : 
           Object.entries(standingsInfo.table).map((team, idx) => {
+            const { name, crestUrl } = team[1].team;
+            const { 
+              position, gamesPlayed, won, draw, lost, points,
+              goalsFor, goalsAgainst, goalDifference
+            } = team[1];
+
             return (
-              <tr key={idx}>
-                {console.log(team[1].team.name)}
+              <tr key={idx} className={`team-data ${position}-pos`}>
+                <td className="data">{position}</td>
+                <td className="data">{name}</td>
+                <td className="data">
+                  <img className="crestUrl" src={crestUrl} alt="team logo" />
+                </td>
+                <td className="data">{gamesPlayed}</td>
+                <td className="data">{won}</td>
+                <td className="data">{draw}</td>
+                <td className="data">{lost}</td>
+                <td className="data">{points}</td>
+                <td className="data">{goalsFor}</td>
+                <td className="data">{goalsAgainst}</td>
+                <td className="data data-last">{goalDifference}</td>
               </tr>
             )
           })
