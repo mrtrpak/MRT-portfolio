6005df9bd70f24e13fdf6906b95de893c5311a94
Michael Trpak <mrtrpak3@gmail.com>
2021-08-12 23:18:50 -0400

    adding more styling for a better look with an added react-icon to replace the arrow

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { AiOutlineArrowRight } from 'react-icons/ai';
 
 import './custom-button.styles.scss';
 
 const CustomButton = ({ route, title }) => (
   <div className="custom-button">
-    <Link to={route}>
+    <Link to={route} className="custom-button-link">
       <span className="custom-button-title">
         {title.toUpperCase()}
+      </span>
+      <span className="custom-button-icon-container">
+        <AiOutlineArrowRight className="custom-button-icon" />
       </span>
     </Link>
   </div>
