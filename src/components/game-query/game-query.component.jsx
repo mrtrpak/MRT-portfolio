3aac29272a9113aad24e4e9def3cf98beaaa1d91
Michael Trpak <mrtrpak3@gmail.com>
2021-08-15 13:02:16 -0400

    updates to styling of game query

--- src/components/game-query/game-query.component.jsx
+++ src/components/game-query/game-query.component.jsx
@@ -4,12 +4,16 @@ import './game-query.styles.scss';
 
 import CustomApiButton from '../custom-api-button/custom-api-button.component';
 
-const GameQuery = () => (
-  <div>
-    <input type="text" />
-    <CustomApiButton title={"Find Game"} 
-      onClick={() => console.log('hello')} />
-  </div>
-);
+const GameQuery = () => {
+  
+  return (
+    <form className="game-query">
+      <input type="text" className="game-text" 
+        id="game-query-input" placeholder="Enter a game title here" />
+      <CustomApiButton btnTitle={"Find Game"} 
+        onClick={e => console.log(e.target.value)} className="game-submit-btn" />
+    </form>
+  );
+};
 
 export default GameQuery;
