2d43723cc84c96d8f628865609799b4063d452a9
Michael Trpak <mrtrpak3@gmail.com>
2021-08-15 13:19:54 -0400

    restructed game query component and finished styling

--- src/components/game-query/game-query.component.jsx
+++ src/components/game-query/game-query.component.jsx
@@ -7,12 +7,12 @@ import CustomApiButton from '../custom-api-button/custom-api-button.component';
 const GameQuery = () => {
   
   return (
-    <form className="game-query">
+    <div className="game-query">
       <input type="text" className="game-text" 
         id="game-query-input" placeholder="Enter a game title here" />
       <CustomApiButton btnTitle={"Find Game"} 
         onClick={e => console.log(e.target.value)} className="game-submit-btn" />
-    </form>
+    </div>
   );
 };
 
