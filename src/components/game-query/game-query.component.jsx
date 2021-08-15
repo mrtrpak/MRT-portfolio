6dc6849c0d0d1999dc47a26d9c3e83e9580f7d3f
Michael Trpak <mrtrpak3@gmail.com>
2021-08-15 12:26:42 -0400

    importing in button to grab query

--- src/components/game-query/game-query.component.jsx
+++ src/components/game-query/game-query.component.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import './game-query.styles.scss';
 
+import CustomApiButton from '../custom-api-button/custom-api-button.component';
+
 const GameQuery = () => (
   <div>
     <input type="text" />
-    
+    <CustomApiButton title={"Find Game"} 
+      onClick={() => console.log('hello')} />
   </div>
 );
 
