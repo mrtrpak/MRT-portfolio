a98e6f46de02f55d19eeef533ec45df376ab4a92
Michael Trpak <mrtrpak3@gmail.com>
2021-08-16 14:31:23 -0400

    added imports and needed files

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { Switch, Route } from 'react-router-dom';
 import './App.scss';
 
 import bgImage from './assets/mntsBG.jpg';
+
 import Header from './components/header/header.component';
+
 import HomePage from './pages/home/homepage.component';
 import HobbiesPage from './pages/hobbies/hobbies.component';
 import ProjectsPage from './pages/projects/projects.components';
 import SkillsPage from './pages/skills/skills.component';
 import SoccerPage from './pages/soccer/soccer.component';
 import GamerPage from './pages/gamer/gamer.component';
+import AntiBoredomPage from './pages/anti-boredom/anti-boredom.component';
+import TravelPage from './pages/travel/travel.component';
 
 function App() {
   return (
@@ -30,6 +34,8 @@ function App() {
         <Route exact path="/skills" component={SkillsPage} />
         <Route exact path="/soccer" component={SoccerPage} />
         <Route exact path="/gamer" component={GamerPage} />
+        <Route exact path="/anti-boredom" component={AntiBoredomPage} />
+        <Route exact path="/travel" component={TravelPage} />
       </Switch>
     </div>
   );
