4865b18da79e90f10250b3a2b6136433dc15c455
Michael Trpak <mrtrpak3@gmail.com>
2021-08-16 14:24:51 -0400

    creating anti boredom page

---
+++ src/pages/anti-boredom/anti-boredom.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './anti-boredom.styles.scss';
+
+const AntiBoredomPage = () => (
+  <div>
+    anti-boredom page
+  </div>
+);
+
+export default AntiBoredomPage;
