d7bae5682341dacfac6ed3ad8a0cde376d77e2c0
Michael Trpak <mrtrpak3@gmail.com>
2021-08-22 21:10:52 -0400

    updates

--- src/components/game-data-modal/game-data-modal.component.jsx
+++ src/components/game-data-modal/game-data-modal.component.jsx
@@ -4,36 +4,56 @@ import YTSearch from 'youtube-api-search';
 import './game-data-modal.styles.scss';
 
 import { youtubeKey } from '../../utils/secret';
+import everyone from '../../assets/esrb-rating-images/everyone.png';
+import everyone10 from '../../assets/esrb-rating-images/everyone10+.png';
+import teen from '../../assets/esrb-rating-images/teen.png';
+import mature from '../../assets/esrb-rating-images/mature.png';
+import adultsOnly from '../../assets/esrb-rating-images/adults-only.png';
+import ratingPending from '../../assets/esrb-rating-images/rating-pending.png';
 import YoutubePlayer from '../youtube-player/youtube-player.component';
 
 const GameDataModal = ({ finalChoice }) => {
-  const [hidden, setHidden] = useState("hidden");
-  const [videoIdArray, setVideoIdArray] = useState([]);
-  
-  useEffect(() => {
-    
-    if (Object.keys(finalChoice).length === 0) {
-      setHidden("hidden");
-    } else {
-      const { name } = finalChoice;
-
-      const youtubeVideoSearch = () => {
-        YTSearch({ key: youtubeKey, term: `${name} official trailer`}, 
-          videos => { videos.map(video => (
-            setVideoIdArray(array => [...array, video.id.videoId])
-          ))}
-        );
-      };
-
-      setHidden("");
-      setVideoIdArray([]);
-      youtubeVideoSearch();
-    }
-  }, [finalChoice]);
-
   const { 
     name, rating, background_image, released, score, esrb_rating, genres, platforms  
   } = finalChoice;
+  const [hidden, setHidden] = useState("hidden");
+  const [videoIdArray, setVideoIdArray] = useState([]);
+  const [esrbSrc, setEsrbSrc] = useState(undefined);
+  const [backgroundImage, setBackgroundImage] = useState(null);
+  const youtubeVideoSearch = () => {
+    YTSearch({ key: youtubeKey, term: `${name} official trailer`}, 
+      videos => { videos.map(video => (
+        setVideoIdArray(array => [...array, video.id.videoId])
+      ))}
+    );
+  };
+
+  youtubeVideoSearch();
+
+  useEffect(() => {
+    if (Object.keys(finalChoice).length === 0) {
+      setHidden("hidden");
+      setVideoIdArray([]);
+    } else {
+      setHidden("");
+    };
+  }, [finalChoice]);
+
+  // if (!esrb_rating) {
+  //   setEsrbSrc(null);
+  // } else if (esrb_rating.slug === "everyone") {
+  //   setEsrbSrc(everyone);
+  // } else if (esrb_rating.slug === "everyone-10-plus") {
+  //   setEsrbSrc(everyone10);
+  // } else if (esrb_rating.slug === "teen") {
+  //   setEsrbSrc(teen);
+  // } else if (esrb_rating.slug === "mature") {
+  //   setEsrbSrc(mature);
+  // } else if (esrb_rating.slug === "adults-only") {
+  //   setEsrbSrc(adultsOnly);
+  // } else if (esrb_rating.slug === "rating-pending") {
+  //   setEsrbSrc(ratingPending);
+  // }
   
   return (
     <div className="game-data-modal" hidden={hidden}>
@@ -41,12 +61,15 @@ const GameDataModal = ({ finalChoice }) => {
       <div className="player-and-ratings">
         <YoutubePlayer videoIdArray={videoIdArray} />
         <div className="game-ratings">
-          <div className="esrb-rating">
-
-          </div>
+          {
+            !esrbSrc ? null : 
+            <div className="esrb-rating">
+              
+            </div>
+          }
           <div className="game-rating">
             <h4 className="game-rating-title">GAME RATING</h4>
-            <p className="game-rating-score">{rating}</p>
+            <p className="game-rating-score">{rating} / 5</p>
           </div>
         </div>
       </div>
